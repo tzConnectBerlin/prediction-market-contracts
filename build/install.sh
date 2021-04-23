e155d8474d1f25bbb29fae0c2bf74dfc34dbcd40
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-04-23 15:37:09 +0200

    Fix to the deployment script

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+account_name="scrooge"
+contract_name="binary_pm"
 burn_cap=2.0
 
 lazy_folder="./bin/lazy"
@@ -7,7 +9,7 @@ lambda_files=${lazy_folder}/*.tz
 
 echo "Installing contract..."
 storage=`cat bin/storage.tz`
-tezos-client originate contract munchlax transferring 0 from alice \
+tezos-client originate contract ${contract_name} transferring 0 from ${account_name} \
 	running bin/main.tz --init "${storage//[$'\t\r\n']}" --burn-cap ${burn_cap} --force
 
 for f in ${lambda_files}
@@ -16,9 +18,9 @@ do
 	lambda_name=${filename%.[^.]*}
 	lambda=`cat ${f}`
 	echo "Installing ${filename} as ${lambda_name}..."
-	tezos-client transfer 0 from alice to munchlax --burn-cap ${burn_cap} \
+	tezos-client transfer 0 from ${account_name} to ${contract_name} --burn-cap ${burn_cap} \
 		--entrypoint installLambda --arg \(Pair\ \"${lambda_name}\"\ "${lambda//[$'\t\r\n']}"\)
 done
 
 echo "Sealing contract..."
-tezos-client transfer 0 from alice to munchlax --burn-cap ${burn_cap} --entrypoint sealContract
+tezos-client transfer 0 from ${account_name} to ${contract_name} --burn-cap ${burn_cap} --entrypoint sealContract
